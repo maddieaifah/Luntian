04eb103d1f46c2d8118fc746fbabda75978ed095
andreilandicho <116428424+andreilandicho@users.noreply.github.com>
2025-06-28 13:53:05 +0800

    after running dotnet run

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a90bb1302a89ac6db750b01570b044b7b8575156")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ebf979db6991d6841ad7f250d158df43a90438b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
